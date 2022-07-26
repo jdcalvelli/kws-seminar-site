d8d95be462c9eee9de4b2947ba312360df2ed0ff
JD Calvelli <jdcalvelli@gmail.com>
2022-07-26 12:26:29 -0500

    adding link to nav bar to bring back home

--- kws-frontend/src/SeminarPage.jsx
+++ kws-frontend/src/SeminarPage.jsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom"
 
+import Nav from "./components/Nav"
+
 function SeminarPage(props) {
     let { id } = useParams()
-    
+
     return (
-        <h1> this page should have the same number as the route here: {id}</h1>
+        <div className="seminarPage">
+            <Nav />
+            <h1> this page should have the same number as the route here: {id}</h1>
+        </div>
     )
 }
 
--- kws-frontend/src/components/Nav.jsx
+++ kws-frontend/src/components/Nav.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 function Nav(props) {
 
     return (
         <div className="Nav">
-            <h1> <a href="#">kws</a> </h1>
+            <h1><Link to="/"> kws </Link></h1>
             <h3> <a href="#">seminars</a> </h3>
             <h3> <a href="#">about</a> </h3>
             <h3> <a href="#">contact</a> </h3>
