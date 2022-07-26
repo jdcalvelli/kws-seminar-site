a31638f88c91bedd686888898d6cd655bf3cedf5
JD Calvelli <jdcalvelli@gmail.com>
2022-07-26 14:57:12 -0500

    adding first pass styling to seminar section

--- kws-frontend/src/components/SeminarSection.jsx
+++ kws-frontend/src/components/SeminarSection.jsx
@@ -5,7 +5,7 @@ import SeminarCard from "./SeminarCard"
 
 function SeminarSection(props) {
 
-    const [cardArrayData, setCardArrayData] = useState({isLoaded: false, cardArray: []})
+    const [cardArrayData, setCardArrayData] = useState({ isLoaded: false, cardArray: [] })
 
     // have to use get effect for on component mount to pull data
     // have to capture both potentials for is loaded return and is not loaded
@@ -20,17 +20,19 @@ function SeminarSection(props) {
             <h1> this will contain individual seminar cards </h1>
             <h1> this will also contain the filtering method somehow </h1>
 
-            { !cardArrayData.isLoaded ? 
-                <h1> Loading ... </h1> :
-                cardArrayData.cardArray.map((item) => {
-                    // console.log(item)
-                    return <SeminarCard key={item.id} 
-                                        id = {item.id}
-                                        imgSrc={"http://localhost:1337" + item.attributes.seminarImage.data.attributes.url} 
-                                        title = {item.attributes.seminarTitle} 
-                                        flavorText={item.attributes.seminarFlavorText}/>
-                })
-            }
+            <div className="cardSection">
+                {!cardArrayData.isLoaded ?
+                    <h1> Loading ... </h1> :
+                    cardArrayData.cardArray.map((item) => {
+                        // console.log(item)
+                        return <SeminarCard key={item.id}
+                            id={item.id}
+                            imgSrc={"http://localhost:1337" + item.attributes.seminarImage.data.attributes.url}
+                            title={item.attributes.seminarTitle}
+                            flavorText={item.attributes.seminarFlavorText} />
+                    })
+                }
+            </div>
 
         </div>
     )
