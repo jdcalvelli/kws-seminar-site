bb1e3577da4a80fadfe9e7efab345d86e337b0b4
JD Calvelli <jdcalvelli@gmail.com>
2022-07-26 11:50:53 -0500

    adding react router and testing second route

--- kws-frontend/src/main.jsx
+++ kws-frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import App from "./App"
 //import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/seminars/:id' element='testing second route'></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
