a800c54ba4455df8449176c536bfae24b2f2f287
JD Calvelli <jdcalvelli@gmail.com>
2022-07-27 12:47:12 -0500

    update hero section to use mantine

--- kws-frontend/src/components/Hero.jsx
+++ kws-frontend/src/components/Hero.jsx
@@ -1,9 +1,13 @@
+import { Center, Stack, Title } from "@mantine/core"
+
 function Hero(props) {
     return (
-        <div className="Hero">
-            <h1> this is seminar </h1>
-            <h3> this is a subtitle for the website </h3>
-        </div>
+        <Center sx={{height: "600px"}}>
+            <Stack align="center" spacing="xl">
+                <Title order={1}>this is seminar</Title>
+                <Title order={3}>this is a subtitle for the site</Title>
+            </Stack>
+        </Center>
     )
 }
 
