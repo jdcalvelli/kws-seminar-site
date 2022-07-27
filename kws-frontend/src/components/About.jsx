c51705e6667d125aceecb492a3e67d9d1fd3bee1
JD Calvelli <jdcalvelli@gmail.com>
2022-07-27 14:31:16 -0500

    adding first pass mantine about paged

--- kws-frontend/src/components/About.jsx
+++ kws-frontend/src/components/About.jsx
@@ -1,9 +1,21 @@
+import { Group, Image, Stack, Text, Title } from "@mantine/core"
+
 function About(props) {
     return (
-        <div className="About">
-            <h1> About Us </h1>
-            <h3> this is where we say some stuff about us </h3>
-        </div>
+        <Stack spacing="xl" style={{height: 600}} justify="center">
+            <Title order={1} mx="xl">About Us</Title>
+            <Group noWrap spacing="xl">
+                <Text mx="xl" style={{width: '50%'}}> The Center for RISC is the brainchild of Steven Levitt, 
+                professor of economics at the University of Chicago and co-author of Freakonomics. 
+                We are not an academic group or research lab. Instead, we investigate today’s 
+                ocial issues by combining unconventional perspectives with empirical data, 
+                generating radical solutions with real-world relevance. We then test and scale those solutions 
+                through a mix of partnerships with academics, nonprofits, government agencies, 
+                international organizations, and private corporations.</Text>
+
+                <Text mx="xl" style={{width: '50%'}}> THIS IS WHERE THE RISC IMAGE WILL GO</Text>
+            </Group>
+        </Stack>
     )
 }
 
