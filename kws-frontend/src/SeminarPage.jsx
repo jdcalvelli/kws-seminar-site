4a110cd06088f2f312fd97f3caa4a49b6fb71f40
JD Calvelli <jdcalvelli@gmail.com>
2022-07-28 13:53:32 -0500

    updating seminar page to have mantine components

--- kws-frontend/src/SeminarPage.jsx
+++ kws-frontend/src/SeminarPage.jsx
@@ -5,9 +5,10 @@ import getSeminar from "./tasks/getSeminar"
 
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
+import { Anchor, Button, Group, Loader, Stack, Title } from "@mantine/core"
 
 function SeminarPage(props) {
-    const [seminarData, setSeminarData] = useState({isLoaded: false, seminarData: {}})
+    const [seminarData, setSeminarData] = useState({ isLoaded: false, seminarData: {} })
 
     let { id } = useParams()
 
@@ -18,19 +19,20 @@ function SeminarPage(props) {
     return (
         <div className="seminarPage">
             <Nav />
-            <h1> this page should have the same number as the route here: {id}</h1>
 
-            {!seminarData.isLoaded ? 
-            <h1>Loading...</h1> :
-            <div className="seminarEmbeds">
-                <h1>{seminarData.seminarData.attributes.seminarTitle}</h1>
-                <iframe src={seminarData.seminarData.attributes.embedURL} frameBorder="0"></iframe>
-                <a href={seminarData.seminarData.attributes.requiredReadingURL}> Required Reading </a>
-                <a href={seminarData.seminarData.attributes.optionalReadingURL}> Optional Reading </a>
-            </div> 
+            {!seminarData.isLoaded ?
+                <Loader size="xl" variant="dots" /> :
+                <Stack>
+                    <Title order={1} m='xl'>{seminarData.seminarData.attributes.seminarTitle}</Title>
+                    <iframe src={seminarData.seminarData.attributes.embedURL} frameBorder="0" style={{padding: 36, height: '80vh'}}></iframe>
+                    <Group p='xl' position="center">
+                        <Anchor href={seminarData.seminarData.attributes.requiredReadingURL}><Button size="xl">required readings</Button></Anchor>
+                        <Anchor href={seminarData.seminarData.attributes.optionalReadingURL}><Button size="xl">optional readings</Button></Anchor>
+                    </Group>
+                </Stack>
             }
 
-            <Footer />
+            <Footer style={{height: 'calc(100% - 80px)'}}/>
         </div>
     )
 }
