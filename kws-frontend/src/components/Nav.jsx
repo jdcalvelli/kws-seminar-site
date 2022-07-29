15e39684bced1defa72613319eee91777000f667
JD Calvelli <jdcalvelli@gmail.com>
2022-07-29 12:58:54 -0500

    removing elements of nav bar that are unusable on seminar page

--- kws-frontend/src/components/Nav.jsx
+++ kws-frontend/src/components/Nav.jsx
@@ -1,21 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { Link as LinkRS } from 'react-scroll'
 
 import { Anchor, Button, Group, Header, Title } from '@mantine/core'
 
+
+
 function Nav(props) {
+
+    let { id } = useParams()
+
+    console.log(id)
 
     return (
 
         <Header height={80}>
-            <Group align="center" position="apart" sx={{height: '100%'}} px="xl">
+            <Group align="center" position="apart" sx={{ height: '100%' }} px="xl">
                 <Title order={1}><Anchor component={Link} to="/">kws</Anchor></Title>
 
-                <Group spacing="xl">
-                    <Title order={3}><LinkRS to="Seminars" spy={true} smooth={true} offset={160} duration={500}>seminars</LinkRS></Title>
-                    <Title order={3}><LinkRS to="About" spy={true} smooth={true} offset={0} duration={500}>about</LinkRS></Title>
-                    <Button radius="xl"><LinkRS to="Contact" spy={true} smooth={true} offset={0} duration={500}>contact</LinkRS></Button>
-                </Group>
+                {id === undefined ?
+                    <Group spacing="xl">
+                        <Title order={3}><LinkRS to="Seminars" spy={true} smooth={true} offset={160} duration={500}>seminars</LinkRS></Title>
+                        <Title order={3}><LinkRS to="About" spy={true} smooth={true} offset={0} duration={500}>about</LinkRS></Title>
+                        <Button radius="xl"><LinkRS to="Contact" spy={true} smooth={true} offset={0} duration={500}>contact</LinkRS></Button>
+                    </Group> :
+                    null
+                }
+
             </Group>
         </Header>
     )
