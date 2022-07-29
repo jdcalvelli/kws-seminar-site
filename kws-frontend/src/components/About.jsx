e6b8246982e341d1e63b8f2173215d20dffa9ffa
JD Calvelli <jdcalvelli@gmail.com>
2022-07-29 10:42:24 -0500

    updating about me section layout

--- kws-frontend/src/components/About.jsx
+++ kws-frontend/src/components/About.jsx
@@ -4,7 +4,7 @@ function About(props) {
     return (
         <Stack spacing="xl" style={{height: 600}} justify="center" id="About">
             <Title order={1} mx="xl">About Us</Title>
-            <Group noWrap spacing="xl" position="apart">
+            <Group noWrap spacing="xl" position="center" align='center'>
                 <Text mx="xl" style={{width: '50%'}}>{props.aboutText}</Text>
 
                 <Image sx={{height: 200, width: 200}} src={props.imgSrc} px="xl"></Image>
