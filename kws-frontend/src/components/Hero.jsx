f6fc51ade1c83bf32aa42501d6f09298fdc68252
JD Calvelli <jdcalvelli@gmail.com>
2022-08-04 14:11:00 -0500

    addressing kara comments on aug 4 pt 1

--- kws-frontend/src/components/Hero.jsx
+++ kws-frontend/src/components/Hero.jsx
@@ -1,11 +1,11 @@
-import { Center, Stack, Title } from "@mantine/core"
+import { Center, Stack, Text, Title } from "@mantine/core"
 
 function Hero(props) {
     return (
         <Center sx={{height: "90vh"}}>
             <Stack align="center" spacing="xl">
                 <Title order={1}>{props.title}</Title>
-                <Title order={3}>{props.subtitle}</Title>
+                <Text size="lg" sx={{maxWidth: 600, textAlign: 'center'}}>{props.subtitle}</Text>
             </Stack>
         </Center>
     )
